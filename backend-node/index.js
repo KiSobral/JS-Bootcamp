b54df6ed688b694e1c4510a08cfd06068c1e7e3f
Hugo Sobral <hugosobral10@gmail.com>
2019-12-19 02:01:14 -0300

    initial structure of forms processing

--- backend-node/index.js
+++ backend-node/index.js
@@ -9,16 +9,22 @@ nunjucks.configure("views", {
   watch: true,
 });
 
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "njk");
 
-const users = ['Thais Rebouças', 'Leonardo Gomes', 'Hugo Sobral'];
+const users = ["Thais Rebouças", "Leonardo Gomes", "Hugo Sobral"];
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   return res.render("list", { users });
 })
 
-app.get('/new', (req, res) => {
+app.get("/new", (req, res) => {
   return res.render("new");
+});
+
+app.post("/create", (req, res) => {
+  users.push(req.body.user)
+  return res.redirect("/")
 });
 
 app.listen(3000);
