5e5e985a6d15df4a7238536b8751a565720a52ae
Hugo Sobral <hugosobral10@gmail.com>
2019-12-19 01:16:46 -0300

    creating dynamic routing and passing params through routes

--- backend-node/index.js
+++ backend-node/index.js
@@ -11,10 +11,14 @@ nunjucks.configure("views", {
 
 app.set("view engine", "njk");
 
-app.get('/:name', (req, res) => {
-  return res.render("list", {
-    name: req.params.name,
-  });
+const users = ['Thais Rebouças', 'Leonardo Gomes', 'Hugo Sobral'];
+
+app.get('/', (req, res) => {
+  return res.render("list", { users });
 })
+
+app.get('/new', (req, res) => {
+  return res.render("new");
+});
 
 app.listen(3000);
